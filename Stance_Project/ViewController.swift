342124cedfc3ee6177aa058ab871dccf3a6ceb6f
yumokharu <112497653+yumokharu@users.noreply.github.com>
2024-06-17 18:11:12 +0900

    Initial Commit

---
+++ Stance_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stance_Project
+//
+//  Created by 차유민 on 6/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
